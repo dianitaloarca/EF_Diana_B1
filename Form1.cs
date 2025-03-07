c0be5f1daf2c531260d16eb6f4e2658e19b67d8e
dianitaloarca <loarca.g.diana.loyola@gmail.com>
2025-03-07 12:53:45 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EF_Diana_B1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+            //Obligar al usuario a elegir dos numeros diferentes y compararlos
+        {
+            if (int.TryParse(textBox1.Text, out int numero))
+            {   if (numero > 0 && < 250)
+                    MessageBox.Show("el número es mayor");
+            }
+        }
+    }
+}
